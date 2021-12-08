4bfe8c5a00482b8247067d1077442f5a9f45d419
cesartaira86 <cesar@ctairay.com>
2021-12-08 17:14:36 -0500

    Invocar metodos con autorizacion para carrito
    Invocar metodos con autorizacion para carrito

--- src/app/pages/cliente/carrito/carrito.component.ts
+++ src/app/pages/cliente/carrito/carrito.component.ts
@@ -11,15 +11,17 @@ export class CarritoComponent implements OnInit {
   carrito : any = [];
   constructor(private readonly ps: PedidoclienteService) { }
 
-  __getCarrito(idUsuario: number){
-    console.log(idUsuario);
-    this.ps.__getCarritoCliente(idUsuario).subscribe((rest: any) =>{
+  __getCarrito(){
+    //console.log(idUsuario);
+    const token  = sessionStorage.getItem("token");
+    const header = { Authorization: 'Bearer ' + "eyJhbGciOiJSUzI1NiIsImtpZCI6IkUyREE5MTM2M0QwMkM2ODYwMTM2NEU1QzhGQUZDMzY1IiwidHlwIjoiYXQrand0In0.eyJuYmYiOjE2MzkwMDEzNDAsImV4cCI6MTYzOTAwNDk0MCwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo1MDAwIiwiYXVkIjoiQVBJLUFQUC1VUEMiLCJjbGllbnRfaWQiOiI3MUJCNzIzNi1DOTdGLTQ2RjgtQTBDQi0zOTVBQTBGQ0FEREYiLCJjbGllbnRfbnVtZXJvX2RvY3VtZW50byI6IjQzNjg1MjcxIiwiY2xpZW50X2NvZGlnb191c3VhcmlvIjoiMSIsImp0aSI6IjBGNTc5QTE2NTBEOTEwRUEwQUQ1QUYxOTREQzdERUJBIiwiaWF0IjoxNjM5MDAxMzQwLCJzY29wZSI6WyJlbWFpbCJdfQ.jhq7oqMkJnVJtiAcu2ZH_FJes_dGpVUi0nQGuUaPe2DCcOCjlcaxS5HXk50TbG0gprsWlqePj0HNN420iZXERi5P86WlOweo96fYZ-bjEiT-cDPS0MMq__y8sTus2wy7FEor1-irHqBKUa5GmpLBeCYcSSvamNlgbQeeBbsUKQyaccINcGDJh4wzRhd4J76E3dEgjg8H9TcsG9xHHN0-Ll4yNCuVGoY2u5i1Y5hdj4kbpSqUNR9ZmYC5We9k9_wWzz-D8fr0g7q_7mO3BjGy-KxFIxFOpCuHPYxKQdiZOSl2QWqjC4LDcDzz6oxg80hHFJjvhDuUU7SEsMRJkWUWfg" };
+    this.ps.__getCarritoCliente(header).subscribe((rest: any) =>{
         this.carrito = rest.data;
         //console.log(rest.data);
     })
   }
   ngOnInit(): void {
-    this.__getCarrito(1);
+    this.__getCarrito();
   }
 
 }
