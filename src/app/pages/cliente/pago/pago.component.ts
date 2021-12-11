8518087ddf3a38c39f9044aad72806ae67a800f3
cesartaira86 <cesar@ctairay.com>
2021-12-10 19:01:58 -0500

    Token

--- src/app/pages/cliente/pago/pago.component.ts
+++ src/app/pages/cliente/pago/pago.component.ts
@@ -23,7 +23,7 @@ export class PagoComponent implements OnInit {
 
   __getMontoCarrito(){
     const token  = sessionStorage.getItem("token");
-    const header = { Authorization: 'Bearer ' + "eyJhbGciOiJSUzI1NiIsImtpZCI6IkUyREE5MTM2M0QwMkM2ODYwMTM2NEU1QzhGQUZDMzY1IiwidHlwIjoiYXQrand0In0.eyJuYmYiOjE2MzkxNzA1ODAsImV4cCI6MTYzOTI0MjU4MCwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo1MDAwIiwiYXVkIjoiQVBJLUFQUC1VUEMiLCJjbGllbnRfaWQiOiI3MUJCNzIzNi1DOTdGLTQ2RjgtQTBDQi0zOTVBQTBGQ0FEREYiLCJjbGllbnRfbnVtZXJvX2RvY3VtZW50byI6IjQzNjg1MjcxIiwiY2xpZW50X2NvZGlnb191c3VhcmlvIjoiMSIsImp0aSI6IkE3MDIzRDc2RkRDOTlENDgxOUNBODJDREMwNjVBQ0FDIiwiaWF0IjoxNjM5MTcwNTgwLCJzY29wZSI6WyJlbWFpbCJdfQ.pTzuhvvcFkQl9wJhUL4MoPX6WJK2jnMU_lxeYlbfzR6BPGIpvivMVnyRjp8g7A2qcXhaiB8jqoAwLhA_y_pnz9msLJ-8XPadTXkex2YsBh-gXO2NoKdwuYjCWixeZjZ_zqaCWXy9D6XJES3XA2MUssl87zE9zlikIOcW7VkC_8tSv1aXbANRhEQ3lG5eSfMFcoIKArpzuqnmvFWvnx3_ZlKjuOgTt60HAbSUjYj0ohzvDMhEoJ-Crr2K5HylJ4A4ez6SIZhs4ZSZKnkxVIRwyTYm4CFu5lBkcuhH_Azcb6YlLVQBX9THFkpGAORB3XRX-00Cr8yCg7yqZ0R5QZcH1A" };
+    const header = { Authorization: 'Bearer ' + token };
     this.ps.__getMontoCarritoCliente(header).subscribe((rest: any) =>{
         this.carrito = rest.data;
         //console.log(rest.data);
@@ -43,7 +43,7 @@ export class PagoComponent implements OnInit {
   }
   __insert() {
     const token  = sessionStorage.getItem("token");
-    const header = { Authorization: 'Bearer ' + "eyJhbGciOiJSUzI1NiIsImtpZCI6IkUyREE5MTM2M0QwMkM2ODYwMTM2NEU1QzhGQUZDMzY1IiwidHlwIjoiYXQrand0In0.eyJuYmYiOjE2MzkxNzA1ODAsImV4cCI6MTYzOTI0MjU4MCwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo1MDAwIiwiYXVkIjoiQVBJLUFQUC1VUEMiLCJjbGllbnRfaWQiOiI3MUJCNzIzNi1DOTdGLTQ2RjgtQTBDQi0zOTVBQTBGQ0FEREYiLCJjbGllbnRfbnVtZXJvX2RvY3VtZW50byI6IjQzNjg1MjcxIiwiY2xpZW50X2NvZGlnb191c3VhcmlvIjoiMSIsImp0aSI6IkE3MDIzRDc2RkRDOTlENDgxOUNBODJDREMwNjVBQ0FDIiwiaWF0IjoxNjM5MTcwNTgwLCJzY29wZSI6WyJlbWFpbCJdfQ.pTzuhvvcFkQl9wJhUL4MoPX6WJK2jnMU_lxeYlbfzR6BPGIpvivMVnyRjp8g7A2qcXhaiB8jqoAwLhA_y_pnz9msLJ-8XPadTXkex2YsBh-gXO2NoKdwuYjCWixeZjZ_zqaCWXy9D6XJES3XA2MUssl87zE9zlikIOcW7VkC_8tSv1aXbANRhEQ3lG5eSfMFcoIKArpzuqnmvFWvnx3_ZlKjuOgTt60HAbSUjYj0ohzvDMhEoJ-Crr2K5HylJ4A4ez6SIZhs4ZSZKnkxVIRwyTYm4CFu5lBkcuhH_Azcb6YlLVQBX9THFkpGAORB3XRX-00Cr8yCg7yqZ0R5QZcH1A" };
+    const header = { Authorization: 'Bearer ' + token };
     //console.log({idProducto:1,cantidad:1});
     console.log(this.pagoForm.value);
     this.ps.__be_insertarPedidoCarrito(header).subscribe((rest: any) => {
